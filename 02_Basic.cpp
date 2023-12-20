92bb296a9c5ee4a87a0b5c910135ad58990a5525
Rajeev Ranjan <135375413+rjranjan2112@users.noreply.github.com>
2023-12-20 15:06:00 +0530

    C++ Basics

---
+++ 02_Basic.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<< "Jiya Rathore" "\n"; 
+    cout<< "Rajeev Ranjan" "\n";
+    cout<< "Rakesh Roushan";
+    return 0;
+}
---
+++ 04_Datatypes.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // int
+    int x=10;
+    cout<< x << "\n";
+    //long
+    long y=67;
+    cout<<y << "\n";
+    // long long
+    long long z=1500000;
+    cout<< z << "\n";
+    // float, double
+    float a= 5.6;
+    float b= 9.6;
+    cout<<a<<"\n"<<b<<"\n";
+
+    double c=4.2837478;
+    cout<<c<<"\n";
+
+    //string
+    string j="Jiya";
+    cout<<j<<"\n";
+
+    //getline
+    string r;
+    getline(cin,r);
+    cout<<r<<"\n";
+
+    //character
+    char ch;
+    cin>>ch;
+    cout<<ch<<"\n";
+
+    
+    
+
+
+
+
+    return 0;
+}
