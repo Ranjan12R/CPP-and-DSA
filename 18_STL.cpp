4d6bc8b36570ff47277fa2b14cd452433b8f6ac0
Rajeev <rjranjan2112@gmail.com>
2024-01-07 15:42:27 +0530

    committed

---
+++ 18_STL.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+void print()
+{
+    cout << "raj";
+}
+int sum(int a, int b)
+{
+    return a + b;
+}
+int main()
+{
+    print();
+    int s = sum(10, 30);
+    cout << s;
+    return 0;
+}
+
+
---
+++ 21_linear_search.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool search(int arr[], int size, int key)
+{
+
+    for (int i = 0; i<size; i++){
+        if (arr[i]==key){
+            return 1;
+        }
+
+    }
+    return 0;
+}
+int main()
+{
+    int arr[10] = {1, 22, -5, -7, 63, 12, 45, 99, -100, 43};
+    cout << "Enter the element to search for " << endl;
+    int key;
+    cin >> key;
+
+    bool found = search(arr, 10, key);
+
+    if (found)
+    {
+        cout << "key is present" << endl;
+    }
+    else
+    {
+        cout << "key is not present" << endl;
+    }
+
+    return 0;
+}
